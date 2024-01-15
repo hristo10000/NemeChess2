ae0f405328bb576b7ba0f9e0ff4be9407caa9b74
Hristo Ivanov <103217093+hristo10000@users.noreply.github.com>
2024-01-15 11:18:03 +0200

    test 5 + a few unused classes deleted

--- NemeChess2/Models/LichessPlayer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NemeChess2.Models
-{
-    public class LichessPlayer
-    {
-        public LichessUser User { get; set; }
-        public int Rating { get; set; }
-        public int RatingDiff { get; set; }
-    }
-}
